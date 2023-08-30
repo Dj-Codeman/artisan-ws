9c38fb3ffa30c90945a214ebb847c01574e09961
Darrion Whitfield <dwhitfield@ramfield.net>
2023-08-30 15:40:37 -0500

    Adding placeholder page

---
+++ under_contruction.php
@@ -0,0 +1,32 @@
+<?php
+// add meta data and css
+// include the header 
+include("parts/header.html");
+?>
+
+<div class="stacked-div">
+    <!-- generic html body -->
+    <link rel="stylesheet" type="text/css" href="../static/css/cover.css">
+    <section class="cover" style="width: 100%; object-position: center bottom; margin-bottom: 40px;">
+        <img src="static/img/vibe.jpg">
+        <div class="image-overlay-2"></div>
+        <div class="cover-content">
+            <h1 style="margin-bottom: 60px;">UNDER CONSTRUCTION
+            </h1>
+
+            <p class="desktop-only" style="padding-left: 20%; width: 60%; text-align: left;">
+                Here, we're hard at work crafting and testing dynamic solutions that are soon to be unveiled. While
+                these demos are still undergoing refinement, this is your exclusive preview into what's on the horizon.
+
+                Stay tuned as we perfect each feature and element, ensuring that when these demos go live, they offer
+                you an exceptional experience. Your patience is greatly appreciated, and we can't wait to share the
+                finalized versions with you.
+            </p>
+        </div>
+    </section>
+</div>
+
+<?php
+//  insert footer data
+include("parts/footer.php");
+?>
